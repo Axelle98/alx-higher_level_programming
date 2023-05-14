200208f80e8f1f119173600a17a0028b5df4c1d5
Axelle98 <axoumoga@gmail.com>
2023-05-14 14:01:53 -0700

    advanced tasks

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 import sys
 import io
+"""
+input = io.TextIOWrapper(sys.stdin.buffer, encoding='utf-8')
+with open(input, "r", encoding="utf-8") as file:
+    for line in file:
+        print(line)
 
-#input = io.TextIOWrapper(sys.stdin.buffer, encoding='utf-8')
-#with open(input, "r", encoding="utf-8") as file:
- #   for line in file:
-  #      print(line)
-
-#input = io.TextIOWrapper(sys.stdin , encoding='utf-8')
-
+input = io.TextIOWrapper(sys.stdin , encoding='utf-8')
+"""
 dictstatus = {}
 totalsize = 0
 totalcount = 0
