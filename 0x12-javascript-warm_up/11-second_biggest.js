b62eda364864dbd779dc4f7dc5da95b4fc94afa7
MOGA Axelle <axoumoga@gmail.com>
2023-06-15 23:45:58 +0200

    Update 11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,6 +1,14 @@
 #!/usr/bin/node
-if (process.argv.length < 4) {
-  console.log(0);
-} else {
-  console.log(process.argv.splice(2, process.argv.length - 1).sort().reverse()[1]);
+//  script that searches the second biggest integer in the list of arguments
+let a = process.argv.map(Number);
+let mx = 0;
+let sc = 0;
+for (let i = 2; i < a.length; i++) {
+  if (a[i] > mx) {
+    sc = mx;
+    mx = a[i];
+  } else if (a[i] > sc && a[i] < mx) {
+    sc = a[i];
+  }
 }
+console.log(sc);
